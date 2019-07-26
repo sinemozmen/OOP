a5233f24260ed1ecbacfe524a1d80120a64ee37e
Section1 <Section1@WISSEN>
2019-07-26 15:51:04 +0300

    OOPProjects

---
+++ 24072019_OOPApp/OOPProject3/LifeDurationInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPProject3
+{
+    public class LifeDurationInfo
+    {
+        public int calculatedAge { get; set; }
+        public double calculatedDays { get; set; }
+        public double calculatedHours { get; set; }
+        public double calculatedMinutes { get; set; }
+        public double calculatedSeconds { get; set; }
+        public double calculatedMiliseconds { get; set; }
+
+
+
+    }
+}
